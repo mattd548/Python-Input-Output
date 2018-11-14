f2981d3701accfb4444869101164e36ece05e1e8
mattd548 <45024604+mattd548@users.noreply.github.com>
2018-11-14 02:17:01 -0500

    Add files via upload

---
+++ Practice 0.py
@@ -0,0 +1,10 @@
+print("******************************")
+print("\tWelcome!")
+print("Please choose a number from the following options")
+print("1\tPlay the game!")
+print("2\tDemo the game!")
+print("3\tExit")
+print("******************************")
+x=input()
+
+
